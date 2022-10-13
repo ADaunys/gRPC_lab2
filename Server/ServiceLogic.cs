48a1735e22c6e51f3400c108d49abf0d0147c84b
ADaunys <augustas.daunys@ktu.edu>
2022-10-13 13:13:18 +0300

    Update ServiceLogic.cs

--- Server/ServiceLogic.cs
+++ Server/ServiceLogic.cs
@@ -8,20 +8,20 @@ using NLog;
 /// </summary>
 public class ServiceLogic
 {
-	/// <summary>
-	/// Logger for this class.
-	/// </summary>
-	Logger log = LogManager.GetCurrentClassLogger();
+    /// <summary>
+    /// Logger for this class.
+    /// </summary>
+    Logger log = LogManager.GetCurrentClassLogger();
 
-	/// <summary>
-	/// Add the arguments, return result.
-	/// </summary>
-	/// <param name="left">Left argument of addition operation.</param>
-	/// <param name="right">Right argument of addition operation.</param>
-	/// <returns>Result of addition operation.</returns>
-	public int Add(int left, int right) 
-	{
-		log.Info($"Add({left}, {right})");
-		return left + right;
-	}
+    /// <summary>
+    /// Add the arguments, return result.
+    /// </summary>
+    /// <param name="left">Left argument of addition operation.</param>
+    /// <param name="right">Right argument of addition operation.</param>
+    /// <returns>Result of addition operation.</returns>
+    public int Add(int left, int right)
+    {
+        log.Info($"Add({left}, {right})");
+        return left + right;
+    }
 }
