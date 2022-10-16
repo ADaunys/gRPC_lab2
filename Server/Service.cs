694db9fd9183bc16e536215d7576de177bd4f1bc
ADaunys <augustas.daunys@ktu.edu>
2022-10-16 14:53:57 +0300

    Squashed commit of the following:
    commit 83d41591c425c45eaa2a908d034d67f96dc7a986
    Author: ADaunys <augustas.daunys@ktu.edu>
    Date:   Sun Oct 16 14:53:30 2022 +0300
    
        Final, works
    
    commit 76f4ee1e12985ebfe2ffabc4a77e335210333e3e
    Author: ADaunys <augustas.daunys@ktu.edu>
    Date:   Sun Oct 16 12:59:42 2022 +0300
    
        testing

--- Server/Service.cs
+++ Server/Service.cs
@@ -12,34 +12,62 @@ using Services;
 /// </summary>
 public class Service : Services.Service.ServiceBase
 {
-	/// <summary>
-	/// Logger for this class.
-	/// </summary>
-	Logger log = LogManager.GetCurrentClassLogger();
+    /// <summary>
+    /// Logger for this class.
+    /// </summary>
+    Logger log = LogManager.GetCurrentClassLogger();
 
-	/// <summary>
-	/// Access lock.
-	/// </summary>
-	private readonly Object accessLock = new Object();
+    /// <summary>
+    /// Access lock.
+    /// </summary>
+    private readonly Object accessLock = new Object();
 
-	/// <summary>
-	/// Service logic implementation.
-	/// </summary>
-	private ServiceLogic logic = new ServiceLogic();
+    /// <summary>
+    /// Service logic implementation.
+    /// </summary>
+    private ServiceLogic logic = new ServiceLogic();
 
+    public override Task<CanSubtractLiquidOutput> CanSubtractLiquid(Empty empty, ServerCallContext context)
+    {
+        log.Info($"Service instance hash code: {this.GetHashCode()}.");
 
-	/// <summary>
-	/// Add input arguments.
-	/// </summary>
-	/// <param name="input">Parts to add.</param>
-	/// <param name="context">Call context.</param>
-	/// <returns>Result of addition operation.</returns>
-	public override Task<AddOutput> Add(AddInput input, ServerCallContext context) {
-		log.Info($"Service instance hash code: {this.GetHashCode()}.");
+        lock (accessLock)
+        {
+            var result = new CanSubtractLiquidOutput { Value = logic.CanSubtractLiquid() };
+            return Task.FromResult(result);
+        }
+    }
 
-		lock( accessLock ) {				
-			var result = new AddOutput { Value = logic.Add(input.Left, input.Right) };
-			return Task.FromResult(result);				
-		}
-	}		
+    public override Task<CanAddLiquidOutput> CanAddLiquid(Empty empty, ServerCallContext context)
+    {
+        log.Info($"Service instance hash code: {this.GetHashCode()}.");
+
+        lock (accessLock)
+        {
+            var result = new CanAddLiquidOutput { Value = logic.CanAddLiquid() };
+            return Task.FromResult(result);
+        }
+    }
+
+    public override Task<AddOutput> SubtractLiquid(Liquid input, ServerCallContext context)
+    {
+        log.Info($"Service instance hash code: {this.GetHashCode()}.");
+
+        lock (accessLock)
+        {
+            var result = new AddOutput { Value = logic.SubtractLiquid(input.Amount) };
+            return Task.FromResult(result);
+        }
+    }
+
+    public override Task<AddOutput> AddLiquid(Liquid input, ServerCallContext context)
+    {
+        log.Info($"Service instance hash code: {this.GetHashCode()}.");
+
+        lock (accessLock)
+        {
+            var result = new AddOutput { Value = logic.AddLiquid(input.Amount) };
+            return Task.FromResult(result);
+        }
+    }
 }
