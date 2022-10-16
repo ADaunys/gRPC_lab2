76f4ee1e12985ebfe2ffabc4a77e335210333e3e
ADaunys <augustas.daunys@ktu.edu>
2022-10-16 12:59:42 +0300

    testing

--- Server/Server.cs
+++ Server/Server.cs
@@ -10,89 +10,107 @@ using NLog;
 /// </summary>
 public class Server
 {
-	/// <summary>
-	/// Logger for this class.
-	/// </summary>
-	Logger log = LogManager.GetCurrentClassLogger();
+    public static int capacity = new Random().Next(0, 100);
+    public static int lowerBound = 0;
+    public static int upperBound = 0;
+    public static bool clientIsActive = false;
 
-	/// <summary>
-	/// Configure loggin subsystem.
-	/// </summary>
-	private void ConfigureLogging()
-	{
-		var config = new NLog.Config.LoggingConfiguration();
+    /// <summary>
+    /// Logger for this class.
+    /// </summary>
+    Logger log = LogManager.GetCurrentClassLogger();
 
-		var console =
-			new NLog.Targets.ConsoleTarget("console")
-			{
-				Layout = @"${date:format=HH\:mm\:ss}|${level}| ${message} ${exception}"
-			};
-		config.AddTarget(console);
-		config.AddRuleForAllLevels(console);
+    /// <summary>
+    /// Configure loggin subsystem.
+    /// </summary>
+    private void ConfigureLogging()
+    {
+        var config = new NLog.Config.LoggingConfiguration();
 
-		LogManager.Configuration = config;
-	}
+        var console =
+            new NLog.Targets.ConsoleTarget("console")
+            {
+                Layout = @"${date:format=HH\:mm\:ss}|${level}| ${message} ${exception}"
+            };
+        config.AddTarget(console);
+        config.AddRuleForAllLevels(console);
 
-	/// <summary>
-	/// Program entry point.
-	/// </summary>
-	/// <param name="args">Command line arguments.</param>
-	public static void Main(string[] args)
-	{
-		var self = new Server();
-		self.Run(args);
-	}
+        LogManager.Configuration = config;
+    }
 
-	/// <summary>
-	/// Program body.
-	/// </summary>
-	/// <param name="args">Command line arguments.</param>
-	private void Run(string[] args) 
-	{
-		//configure logging
-		ConfigureLogging();
+    /// <summary>
+    /// Program entry point.
+    /// </summary>
+    /// <param name="args">Command line arguments.</param>
+    public static void Main(string[] args)
+    {
+        var self = new Server();
+        self.Run(args);
+    }
 
-		//indicate server is about to start
-		log.Info("Server is about to start");
+    /// <summary>
+    /// Program body.
+    /// </summary>
+    /// <param name="args">Command line arguments.</param>
+    private void Run(string[] args)
+    {
+        //configure logging
+        ConfigureLogging();
 
-		//start the server
-		StartServer(args);
-	}
+        //indicate server is about to start
+        log.Info("Server is about to start");
 
-	/// <summary>
-	/// Starts integrated server.
-	/// </summary>
-	/// <param name="args">Command line arguments.</param>
-	private void StartServer(string[] args)
-	{
-		//create web app builder
-		var builder = WebApplication.CreateBuilder(args);
+        //start the server
+        StartServer(args);
 
-		//configure integrated server
-		builder.WebHost.ConfigureKestrel(opts => {
-			opts.Listen(IPAddress.Loopback, 5000);
-		});
+        if (clientIsActive)
+        {
+            log.Info("Client is working...");
+            Thread.Sleep(2000);
+            Server.clientIsActive = false;
+        }
+        lowerBound = new Random().Next(0, 50);
+        upperBound = new Random().Next(lowerBound + 1, 100);
+        log.Info("Bounds changed to: " + lowerBound + " " + upperBound + " and current capacity is: " + capacity);
+        Thread.Sleep(4000);
+    }
+
+    /// <summary>
+    /// Starts integrated server.
+    /// </summary>
+    /// <param name="args">Command line arguments.</param>
+    private void StartServer(string[] args)
+    {
+        //create web app builder
+        var builder = WebApplication.CreateBuilder(args);
+
+        //configure integrated server
+        builder.WebHost.ConfigureKestrel(opts =>
+        {
+            opts.Listen(IPAddress.Loopback, 5000);
+        });
 
 
-		//add support for GRPC services
-		builder.Services.AddGrpc();
+        //add support for GRPC services
+        builder.Services.AddGrpc();
 
-		//add the actural services
-		builder.Services.AddSingleton(new Service());	
+        //add the actural services
+        builder.Services.AddSingleton(new Service());
 
-		//build the server
-		var app = builder.Build();
+        //build the server
+        var app = builder.Build();
 
-		//turn on request routing
-		app.UseRouting();
+        //turn on request routing
+        app.UseRouting();
 
-		//configure routes
-		app.UseEndpoints(ep => {
-			ep.MapGrpcService<Service>();
-		});
-		
-		//run the server
-		app.Run();
-		// app.RunAsync(); //use this if you need to implement background processing in the main thread
-	}
+        //configure routes
+        app.UseEndpoints(ep =>
+        {
+            ep.MapGrpcService<Service>();
+        });
+
+        //run the server
+        //app.Run();
+        app.RunAsync(); //use this if you need to implement background processing in the main thread
+    }
 }
--- Server/Service.cs
+++ Server/Service.cs
@@ -12,34 +12,62 @@ using Services;
 /// </summary>
 public class Service : Services.Service.ServiceBase
 {
-	/// <summary>
-	/// Logger for this class.
-	/// </summary>
-	Logger log = LogManager.GetCurrentClassLogger();
+    /// <summary>
+    /// Logger for this class.
+    /// </summary>
+    Logger log = LogManager.GetCurrentClassLogger();
 
-	/// <summary>
-	/// Access lock.
-	/// </summary>
-	private readonly Object accessLock = new Object();
+    /// <summary>
+    /// Access lock.
+    /// </summary>
+    private readonly Object accessLock = new Object();
 
-	/// <summary>
-	/// Service logic implementation.
-	/// </summary>
-	private ServiceLogic logic = new ServiceLogic();
+    /// <summary>
+    /// Service logic implementation.
+    /// </summary>
+    private ServiceLogic logic = new ServiceLogic();
 
+    public Task<AddOutput> CanSubtractLiquid(ServerCallContext context)
+    {
+        log.Info($"Service instance hash code: {this.GetHashCode()}.");
 
-	/// <summary>
-	/// Add input arguments.
-	/// </summary>
-	/// <param name="input">Parts to add.</param>
-	/// <param name="context">Call context.</param>
-	/// <returns>Result of addition operation.</returns>
-	public override Task<AddOutput> Add(AddInput input, ServerCallContext context) {
-		log.Info($"Service instance hash code: {this.GetHashCode()}.");
+        lock (accessLock)
+        {
+            var result = new AddOutput { Value = logic.CanSubtractLiquid() ? 1 : 0 };
+            return Task.FromResult(result);
+        }
+    }
 
-		lock( accessLock ) {				
-			var result = new AddOutput { Value = logic.Add(input.Left, input.Right) };
-			return Task.FromResult(result);				
-		}
-	}		
+    public Task<AddOutput> CanAddLiquid(ServerCallContext context)
+    {
+        log.Info($"Service instance hash code: {this.GetHashCode()}.");
+
+        lock (accessLock)
+        {
+            var result = new AddOutput { Value = logic.CanAddLiquid() ? 1 : 0 };
+            return Task.FromResult(result);
+        }
+    }
+
+    public Task<AddOutput> SubtractLiquid(AddInput input, ServerCallContext context)
+    {
+        log.Info($"Service instance hash code: {this.GetHashCode()}.");
+
+        lock (accessLock)
+        {
+            var result = new AddOutput { Value = logic.SubtractLiquid(input.Amount) };
+            return Task.FromResult(result);
+        }
+    }
+
+    public Task<AddOutput> AddLiquid(AddInput input, ServerCallContext context)
+    {
+        log.Info($"Service instance hash code: {this.GetHashCode()}.");
+
+        lock (accessLock)
+        {
+            var result = new AddOutput { Value = logic.AddLiquid(input.Amount) };
+            return Task.FromResult(result);
+        }
+    }
 }
